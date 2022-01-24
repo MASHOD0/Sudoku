5b98fd69b719b6637ba500b18241e06e90b093f0
MASHOD0 <mashhood2002@gmail.com>
2022-01-25 00:30:18 +0530

    UserInterfaceImpl.java

--- src/sudoku/SudokuApplication.java
+++ src/sudoku/SudokuApplication.java
@@ -1,5 +1,6 @@
 package sudoku;
 
+import com.wiseassblog.sudoku.userinterface.UserInterfaceImpl;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
