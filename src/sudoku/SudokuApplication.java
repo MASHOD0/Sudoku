a9f4f987a729d9f0238f0492f9b53061c7ba7a44
MASHOD0 <mashhood2002@gmail.com>
2022-01-24 20:43:21 +0530

    add userinterface.IUserinterfaceContract in SudokuApplication.java

--- src/sudoku/SudokuApplication.java
+++ src/sudoku/SudokuApplication.java
@@ -5,19 +5,27 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sudoku.userinterface.IUserInterfaceContract;
+
+import java.io.IOException;
 
 public class SudokuApplication extends Application {
+    private IUserInterfaceContract.View uiImpl;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        uiImpl = new UserInterfaceImpl(primaryStage);
+        try {
+            SudokuBuildLogic.build(uiImpl);
+        }catch(IOException e){
+            e.printStackTrace();
+            throw e;
+        }
     }
 
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }
